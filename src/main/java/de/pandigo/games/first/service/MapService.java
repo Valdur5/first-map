a43ccf10ff708f13e9c70a782bea2393b122678a
Valentin Durst <ValentinDurst@gmail.com>
2018-08-18 12:48:09 +0200

    Added comments to the MapService interface.

--- src/main/java/de/pandigo/games/first/service/MapService.java
+++ src/main/java/de/pandigo/games/first/service/MapService.java
@@ -2,6 +2,15 @@ package de.pandigo.games.first.service;
 
 import de.pandigo.games.first.entity.Position;
 
+/**
+ * Service for providing map related operations
+ */
 public interface MapService {
+    /**
+     * Method for locating a object in the map.
+     *
+     * @param objectId The object id which should be located in the map
+     * @return The Position object containing all location based information about the object
+     */
     Position getPosition(long objectId);
 }
